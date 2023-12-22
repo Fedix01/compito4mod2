faa6ba7f5def0b43a5b3f1e7b631f99bd8934824
“Federico <federicomanna01@yahoo.it>
2023-12-22 21:13:52 +0100

    added section and js functions

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let btn = document.querySelectorAll(".btn-secondary");
     console.log(btn)
-    let section = document.querySelectorAll(".card");
-    console.log(section)
-
+    let firstSection = document.querySelectorAll("#firstRow");
+    console.log(firstSection)
+    let secondSection = document.querySelectorAll("#secondRow");
+    console.log(secondSection)
     // btn.addEventListener("click", function () {
     //     if (section.style.display === "flex") {
     //         section.style.display === "none"
@@ -15,11 +16,21 @@ document.addEventListener("DOMContentLoaded", function () {
     // btn.addEventListener("click", () => {
     //     section.classList.toggle("collapsed")
     // })
-    btn.forEach(elBtn => {
-        section.forEach(element => {
-            elBtn.addEventListener("click", () => {
-                element.classList.toggle("collapsed")
-            })
+    // btn.forEach(elBtn => {
+    //     section.forEach(element => {
+    //         elBtn.addEventListener("click", () => {
+    //             element.classList.toggle("collapsed")
+    //         })
+    //     });
+    // });
+    btn[0].addEventListener("click", () => {
+        firstSection.forEach(element => {
+            element.classList.toggle("collapsed")
         });
-    });
+    })
+    btn[1].addEventListener("click", () => {
+        secondSection.forEach(element => {
+            element.classList.toggle("collapsed")
+        });
+    })
 })
